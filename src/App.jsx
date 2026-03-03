a437786ab81a4f1c37b81955d54ef9d6bcc9b1f4
MmakgadiLetsela <mmakgadi.letsela@gmail.com>
2026-03-03 11:16:32 +0200

    add function responsible for the state and management of the app as a whole

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+import {useEffect, useState} from 'react';
+import PodcastGrid from './components/PodcastGrid.jsx';
+import {genres} from './data.js';
+import {fetchPodcasts} from './api/fetchPodcasts.js';
+import Header from './components/Header.jsx';
 
-export default App
+export default function App () {
+    const [podcasts, setPodcasts] = useState([]);
+    const [loading, setLoading] = useState (true);
+    const [error, setError] = useState(null);
+
+    useEffect (() => {
+        fetchPodcasts (setPodcasts, setError, setLoading);
+    }, []);
+
+    return (
+        <>
+        <Header/>
+        <main>
+            {loading && (
+                <div className="message-container">
+                    <p className="loading-spinner">
+                        Loading podcasts...
+                    </p>
+                </div>
+            )}
+
+            {error && (
+                <div className="message-container">
+                    <div className="error-message">
+                        Error occurred while fetching podcasts : {error}
+                    </div>
+                </div>
+            )}
+
+            {!loading && !error && (
+                <PodcastGrid podcast = {podcasts} genres= {genres}/>
+            )}
+
+        </main>
+        </>
+
+    );
+
+  }
