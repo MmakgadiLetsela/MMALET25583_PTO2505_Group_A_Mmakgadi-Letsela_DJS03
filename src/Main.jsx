7fac54e3c9231b77e96818be3c03f10c620554fd
MmakgadiLetsela <mmakgadi.letsela@gmail.com>
2026-03-03 11:29:02 +0200

    add entry point for the app and the corresponding jsdoc comments

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+/**
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    * Entry point for the application, responsible for rendering the root App component to the DOM
+    * Ensure the React component mounts and displays properly in the browser.
+    * @principle SRP - Single Responsibility Principle: This file is solely responsible for rendering the root component of the application.
+
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
