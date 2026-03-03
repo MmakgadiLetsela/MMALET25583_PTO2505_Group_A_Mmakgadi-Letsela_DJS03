0e51a2946b9839733dbd87d22cd4d58468cba041
MmakgadiLetsela <mmakgadi.letsela@gmail.com>
2026-03-03 11:09:52 +0200

    add header function responsible for the navigation bar of the app and visual start to the app

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header () {
+    return (
+        <header>
+            <h1 className="app-title">
+                PodSURGE
+            </h1>
+                <span className="podcast-icon" role="img" aria-label="podcast icon">🎙️</span>
+        </header>
+    )
+}
+
