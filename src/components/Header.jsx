f69c353214f694dab76e9a138835a7cd508e7447
MmakgadiLetsela <mmakgadi.letsela@gmail.com>
2026-03-03 11:11:07 +0200

    add jsdoc comments for header function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,12 @@
+/**.
+ * Header component for the PodSURGE application. 
+ * @function Header
+ * @returns {JSX.Element} A header element containing the application title and an icon.
+ * @principle SRP -Single Responsibility Principle: this component only handles displaying the header, which includes the title and an icon.
+ * It does not handle any state or logic.
+ */ 
+
+
 export default function Header () {
     return (
         <header>
