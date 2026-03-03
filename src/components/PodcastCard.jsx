bdd277f5f0c1d90b57c333029e3c271f5160d090
MmakgadiLetsela <mmakgadi.letsela@gmail.com>
2026-03-03 11:13:02 +0200

    add jsdoc comments for podcast card function

--- src/components/PodcastCard.jsx
+++ src/components/PodcastCard.jsx
@@ -1,3 +1,11 @@
+/**
+ * PodcastCard component to display individual podcast details.
+ * @function PodcastCard
+ * @prop {Array} podcast -The podcast data containing details such as image and title fetched from the API, passed down from the parent component.
+ * @prop {Array} genres - The list of all genres to map genre IDs to names.
+ * @returns {JSX.Element} A card component displaying the podcast's image, title, seasons, last updated date, and genres.
+ * @principle SRP - Single Responsibility Principle: This component is solely responsible for rendering the details of a single podcast in a card format.
+ */
 
 
 export default function PodcastCard ({podcast,genres}) {
